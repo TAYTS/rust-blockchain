8362df8d66940121682f23e7ffde4e09e45bcca9
TAYTS <taytzushieh@gmail.com>
2025-05-18 15:56:34 +0800

    feat: add new method to TXOutput

--- src/transaction.rs
+++ src/transaction.rs
@@ -159,9 +159,21 @@ impl TXOutput {
         output
     }
 
+    pub fn get_value(&self) -> i32 {
+        self.value
+    }
+
+    pub fn get_pub_key_hash(&self) -> &[u8] {
+        self.pub_key_hash.as_slice()
+    }
+
     fn lock(&mut self, address: &str) {
         let payload = base58_decode(address);
         let pub_key_hash = payload[1..payload.len() - wallet::ADDRESS_CHECK_SUM_LEN].to_vec();
         self.pub_key_hash = pub_key_hash;
+    }
+
+    pub fn is_locked_with_key(&self, pub_key_hash: &[u8]) -> bool {
+        self.pub_key_hash.eq(pub_key_hash)
     }
 }
